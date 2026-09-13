80145f98fa7c32cf46b853f5d4a37549b575d500
codenay-pixel <codenay-pixel@users.noreply.github.com>
2026-09-13 19:16:56 +0700

    Perbaiki footer supaya nempel di bawah viewport (sticky footer)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,11 +49,13 @@
                 {{ $slot }}
             </main>
 
-            <footer class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 text-center text-xs text-gray-400">
-                &copy; {{ date('Y') }}
-                {{ \Illuminate\Support\Str::title(config('instansi.satker')) }} &mdash;
-                {{ \Illuminate\Support\Str::title(config('instansi.direktorat')) }},
-                {{ \Illuminate\Support\Str::title(config('instansi.kementerian')) }}
+            <footer class="mt-10 border-t border-gray-200">
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-6 pb-8 text-center text-xs text-gray-400">
+                    &copy; {{ date('Y') }}
+                    {{ \Illuminate\Support\Str::title(config('instansi.satker')) }} &mdash;
+                    {{ \Illuminate\Support\Str::title(config('instansi.direktorat')) }},
+                    {{ \Illuminate\Support\Str::title(config('instansi.kementerian')) }}
+                </div>
             </footer>
         </div>
     </body>
